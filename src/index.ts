2d8edacdce5eaf03b36776f68de3b3021a581adb
edvardchen <pigeon73101@gmail.com>
2020-01-09 15:22:47 +0800

    chore: init

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         Jenkins Google 自动刷新登录
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  const loginGroup = document.querySelector('.login');
+
+  if (!loginGroup) {
+    throw new Error('can not find login button');
+  }
+
+  const links = loginGroup.querySelectorAll('a');
+  if (!links) {
+    throw new Error('can not find login button');
+  }
+
+  if (links.length > 1) {
+    // 超过一个按钮，说明已经登录
+    // 10s  刷新页面检查一遍
+    setTimeout(() => {
+      location.reload();
+    }, 10000);
+    return;
+  }
+
+  links[0].click();
+})();
